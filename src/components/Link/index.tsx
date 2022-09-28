b7f62cae0af6fc131eb36d82a7b736a6c1fc2e94
ELMO BRAUM <ELMO.BRAUM@intranet.sooretama.es.gov.br>
2022-09-28 14:57:05 -0300

    correção do projeto

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -2,7 +2,7 @@ import NextLink from 'next/link';
 import Style from './style';
 
 export default function Link() {
-    const URL = 'https://https://www.linkedin.com/in/elmo-braun-b34873169/';
+    const URL = 'https:www.linkedin.com/in/elmo-braun-Dev';
     return (
         <Style>
             <NextLink href={URL} passHref>
